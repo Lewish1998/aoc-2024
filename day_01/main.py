0378b61f6ae2e65a018d968314688fbe1163c503
Lewis Halstead <lewishalstead5@gmail.com>
2024-12-02 14:32:27 +0000

    d1 p2

--- day_01/main.py
+++ day_01/main.py
@@ -1,3 +1,4 @@
+# Part One
 left = []
 right = []
 
@@ -14,4 +15,35 @@ new_list = []
 for i in range(len(left)):
     new_list.append(left[i] - right[i]) if left[i] > right[i] else new_list.append(right[i] - left[i])
 
-print(sum(new_list))
+# print(sum(new_list))
+
+
+# Part Two
+# Idea 1
+# obj = lambda: None
+# for i in range(len(left)):
+#     if left[i] in right:
+#         print(f"{left[i]}: {right.count(left[i])}")
+#         field = left[i]
+#         obj.field = right.count(left[i])
+
+# print(obj)
+        
+# Idea 2
+list = []
+for i in range(len(left)):
+    if left[i] in right:
+        sub = [left[i], right.count(left[i])]
+        list.append(sub)
+
+print(list)
+
+answer_list = []
+for i in list:
+    j = i[0] * i[1]
+    answer_list.append(j)
+
+print(answer_list)
+
+answer = sum(answer_list)
+print(answer)
